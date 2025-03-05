940554fbe031deb6071663e9aa5027b10cd2c242
Daniel Echeverria <alejandro.garciae@uanl.edu.mx>
2025-03-05 11:32:44 -0600

    Actualizando al infromacion de los archivos

--- src/app.py
+++ src/app.py
@@ -30,19 +30,35 @@ def inventario():
 
 @app.route('/productos')
 def productos():
+    
     return render_template('productos.html')
 
 @app.route('/proveedores')
 def proveedores():
-    return render_template('proveedores.html')
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT nombre, correo, direccion, telefono, producto FROM Proveedores")  # Ajusta con tu tabla y columnas
+    datos = cursor.fetchall()
+    conn.close()
+    return render_template('proveedores.html', datos = datos)
 
 @app.route('/remesa')
 def remesa():
-    return render_template('remesa.html')
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT proveedor, codigoelemento, modelo, nombre, num_entradas, factura, fecha FROM remesa")  # Ajusta con tu tabla y columnas
+    datos = cursor.fetchall()
+    conn.close()
+    return render_template('remesa.html', datos = datos)
 
 @app.route('/ventas')
 def ventas():
-    return render_template('ventas.html')
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT paciente, graduacion, armason, tratamiento, anticipo, adeudo, fecha FROM ventas")  # Ajusta con tu tabla y columnas
+    datos = cursor.fetchall()
+    conn.close()
+    return render_template('ventas.html', datos = datos)
 
 
 if __name__ == '__main__':
